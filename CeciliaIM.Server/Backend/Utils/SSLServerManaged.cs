cc271de70ee85f14367eeb91c68b7786d0bf991b
MasterYuan418 <yuan@risconn.com>
2021-10-01 09:15:47 +0800

    添加项目文件。

---
+++ CeciliaIM.Server/Backend/Utils/SSLServerManaged.cs
@@ -0,0 +1,65 @@
+﻿using NetCoreServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeciliaIM.Server.Backend.Utils
+{
+    class ChatSession : SslSession
+    {
+        public ChatSession(SslServer server) : base(server) { }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($"Chat SSL session with Id {Id} connected!");
+        }
+
+        protected override void OnHandshaked()
+        {
+            Console.WriteLine($"Chat SSL session with Id {Id} handshaked!");
+
+            // Send invite message
+            string message = "Hello from SSL chat! Please send a message or '!' to disconnect the client!";
+            Send(message);
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Chat SSL session with Id {Id} disconnected!");
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
+            Console.WriteLine("Incoming: " + message);
+
+            // Multicast message to all connected sessions
+            Server.Multicast(message);
+
+            // If the buffer starts with '!' the disconnect the current session
+            if (message == "!")
+                Disconnect();
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat SSL session caught an error with code {error}");
+        }
+    }
+
+    class ChatServer : SslServer
+    {
+        public ChatServer(SslContext context, IPAddress address, int port) : base(context, address, port) { }
+
+        protected override SslSession CreateSession() { return new ChatSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat SSL server caught an error with code {error}");
+        }
+    }
+}
